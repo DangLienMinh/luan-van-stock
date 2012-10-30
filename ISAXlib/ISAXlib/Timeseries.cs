942953cf8af8882da9ebdecd4c00a04926a991b2
quangvietntd@gmail.com <quangvietntd@gmail.com@4337e458-4450-cc61-f575-8856421c4208>
2012-10-30 17:10:07 +0000

    Tạo class Timeseries.cs và viết một số hàm cho Timeseries.cs

---
+++ ISAXlib/ISAXlib/Timeseries.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+namespace ISAXlib
+{
+    class Timeseries
+    {
+        private List<TPoint> series;
+
+        public Timeseries()
+        {
+            series = new List<TPoint>();
+        }
+
+        public Timeseries(double[] values, long[] tstamps)
+        {
+            series = new List<TPoint>();
+            if (values.Length == tstamps.Length)
+            {
+                for (int i = 0; i < values.Length; i++)
+                    series.Add(new TPoint(values[i], tstamps[i]));
+            }
+            else
+            {
+                throw new System.ArgumentException("The lengths of the values and timestamps arrays are not equal!");
+            }
+        }
+
+        public Timeseries(double[] values, long[] tstamps, double nanValue)
+        {
+            series = new List<TPoint>();
+            if (values.Length == tstamps.Length)
+            {
+                for (int i = 0; i < values.Length; i++)
+                    if (nanValue == values[i])
+                        series.Add(new TPoint((0.0D / 0.0D), tstamps[i]));
+                    else
+                        series.Add(new TPoint(values[i], tstamps[i]));
+            }
+            else
+            {
+                throw new System.ArgumentException("The lengths of the values and timestamps arrays are not equal!");
+            }
+        }
+
+        public void add(TPoint p)
+        {
+            series.Add(p);
+        }
+
+ 
+
+    }
+}
