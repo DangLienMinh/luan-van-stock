acec9a8442ef7dde6fe06467fd0893df092246e9
quangvietntd@gmail.com <quangvietntd@gmail.com@4337e458-4450-cc61-f575-8856421c4208>
2012-10-30 14:03:34 +0000

    Tạo project ISAXlib và tạo class TPoint.cs, viết một số hàm trong class TPoint.cs

---
+++ ISAXlib/ISAXlib/TPoint.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ISAXlib
+{
+    public class TPoint
+    {
+        private double value;
+        private long tstamp;
+
+        public TPoint(double value, long tstamp) // tstamp là tổ hợp ngày và giờ
+        {
+            this.value = value;
+            this.tstamp = tstamp;
+        }
+
+        public double value()
+        {
+            return value;
+        }
+
+        public long tstamp()
+        {
+            return tstamp;
+        }
+
+        public void setValue(double newValue)
+        {
+            value = newValue;
+        }
+
+        public long setTstamp(long newTstamp)
+        {
+            tstamp = newTstamp;   
+        }
+
+        public Boolean equals(Object o) // hàm kiểm tra một object có phải là TPoint không
+        {
+            if (o is TPoint) // từ khóa "is" dùng để kiểm tra một object có phải là một instance của object khác hay không
+            {
+                TPoint tp = (TPoint)o;
+                if (value = tp.value() && tstamp = tp.tstamp())
+                    return true;
+            }
+            return false;
+        }
+
+        public int compareTo(TPoint o) // hàm so sánh hai TPoint, ưu tiên so sánh theo tstamp
+        {
+            if (value == o.value() && tstamp == o.tstamp())
+                return 0;
+            if (tstamp > o.tstamp())
+                return 1;
+            if (tstamp < o.tstamp())
+                return -1;
+            return value <= o.value() ? -1 : 1;
+        }
+
+    }
+}
